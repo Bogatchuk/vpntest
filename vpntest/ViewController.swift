f02ca8166874681ebe323f5cf34050a947db21ff
Bogatchuk Roman <41511710+Bogatchuk@users.noreply.github.com>
2023-09-14 14:50:24 +0300

    Initial Commit

---
+++ vpntest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vpntest
+//
+//  Created by Roma Bogatchuk on 14.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
